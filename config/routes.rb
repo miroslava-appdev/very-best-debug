a51fa475138ff8fe47d94a09ab0f953c7db20b3c
Miroslava Osorio <miroslavaosorio1@gmail.com>
2023-05-02 20:23:13 +0000

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-# home
+  # home
   get("/", { :controller => "users", :action => "index" })
 
-#users
+  #users
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:username", { :controller => "users", :action => "show" })
   get("/insert_user_record", { :controller => "users", :action => "create" })
@@ -11,11 +11,10 @@ Rails.application.routes.draw do
   #venues
   get("/venues", { :controller => "venues", :action => "index" })
   get("/insert_venue_record", { :controller => "venues", :action => "create" })
-  get("/venue/:an_id", { :controller => "venues", :action => "show" })
-  post("/update_venue/:the_id", { :controller => "venues", :action => "update" })
+  get("/venues/:an_id", { :controller => "venues", :action => "show" })
+  get("/update_venue/:the_id", { :controller => "venues", :action => "update" })
   get("/delete_venue/:id_to_delete", { :controller => "venues", :action => "destroy" })
-  
+
   #comments
   get("/insert_comment_record", { :controller => "comments", :action => "create" })
- 
 end
