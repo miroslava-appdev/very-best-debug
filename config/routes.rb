cf058914a1ac5ef36249c3330a8ecfd0d00baa86
Miroslava Osorio <miroslavaosorio1@gmail.com>
2023-05-02 05:20:00 +0000

    Updated comment section

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
   get("/insert_user_record", { :controller => "users", :action => "create" })
   get("/update_user/:user_id", { :controller => "users", :action => "update" })
 
+  #venues
   get("/venues", { :controller => "venues", :action => "index" })
   get("/insert_venue_record", { :controller => "venues", :action => "create" })
-  get("/venue/:an_id", { :controller => "venue", :action => "show" })
-  post("/update_venue/:the_id", { :controller => "application", :action => "update" })
-  get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
+  get("/venue/:an_id", { :controller => "venues", :action => "show" })
+  post("/update_venue/:the_id", { :controller => "venues", :action => "update" })
+  get("/delete_venue/:id_to_delete", { :controller => "venues", :action => "destroy" })
   
+  #comments
   get("/insert_comment_record", { :controller => "comments", :action => "create" })
  
 end
